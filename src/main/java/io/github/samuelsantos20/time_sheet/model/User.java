4aa536537f3205031329c3090166e1cfa20a10c1
Samuel Santos Miranda <138618261+SamuelSantos20@users.noreply.github.com>
2025-02-01 07:38:35 -0300

    User class created. Disregard last comment
    Classe Usuário criada.

---
+++ src/main/java/io/github/samuelsantos20/time_sheet/model/User.java
@@ -0,0 +1,50 @@
+package io.github.samuelsantos20.time_sheet.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "password", length = 8, nullable = false, unique = false, updatable = true)
+    private String password;
+
+    @Column(name = "registration", length = 7, updatable = false, unique = false, nullable = false)
+    private String registration;
+
+    @Column(name = "employee_or_manager_id", unique = false, nullable = false, updatable = false,)
+    private UUID user_id;
+
+    
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        User user = (User) o;
+        return getId() != null && Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
